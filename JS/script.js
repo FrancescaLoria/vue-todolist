a4c10fc0b76a2f4f1a8877d7bbdb6076d3657745
FrancescaLoria <francesca.loria90@hotmail.it>
2023-03-30 19:45:09 +0200

    milestone 1

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,27 @@
+const { createApp } = Vue;
 
+createApp({
+  data() {
+    return {
+      todos: [
+        {
+          text: "Perdere peso",
+          done: false,
+        },
+        {
+          text: "Smettere di fumare",
+          done: false,
+        },
+        {
+          text: "Iscrivermi a Boolean",
+          done: true,
+        },
+        {
+          text: "Fare più sport",
+          done: true,
+        },
+      ],
+    };
+  },
+  methods: {},
+}).mount("#app");
